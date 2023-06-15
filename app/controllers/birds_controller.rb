27a7aa71cc0612db4bc23ddb38c11639ba5f52a7
MegDacus <124758848+MegDacus@users.noreply.github.com>
2023-06-15 15:57:33 +1000

    Added update route and functionality, added like incrementing method

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -18,14 +18,38 @@ class BirdsController < ApplicationController
     if bird
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      error
+    end
+  end
+
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird, status: :accepted
+    else  
+      error
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird, status: :accepted
+    else  
+      error
     end
   end
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
+  end
+
+  def error
+    render json: {error: "Bird not found"}, status: :not_found
   end
 
 end
